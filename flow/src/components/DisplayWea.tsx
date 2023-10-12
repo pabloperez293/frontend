f4c93249f5b2356ed901a1c1d0b6c601472be144
pablo <paperezpablo@gmail.com>
2023-10-12 10:29:25 -0300

    Estilos

--- flow/src/components/DisplayWea.tsx
+++ flow/src/components/DisplayWea.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MainWrapper } from './styled.module';
-import { ImSearch } from 'react-icons'
+import { CgSearch } from "react-icons/cg"
+import { WiHumidity } from "react-icons/wi"
+import { GiBarbedSun } from "react-icons/gi"
 
 const DisplayWea = () => {
   return (
@@ -8,13 +10,39 @@ const DisplayWea = () => {
       <div className="container">
         <div className="searchArea">
           <input type="text" placeholder='coloque el pais' />
-        </div>
-        <div className="searchCircle">
-          <ImSearch></ImSearch>
+          <div className="searchCircle">
+            <CgSearch className='searchIcon' />
+          </div>
         </div>
 
+        <div className="weatherArea">
+          <h1>Argentina</h1>
+          <span> buenos aires</span>
+          <div className="icon">
+            icon
+          </div>
+          <h1>20c</h1>
+          <h2>Soleado</h2>
         </div>
-        
+
+        <div className="bottomInfoArea">
+          <div className="humidityLevel">
+            <WiHumidity className='windIcon' />
+            <div className="humidInfo">
+              <h1>20%</h1>
+              <p>Humedad</p>
+            </div>
+          </div>
+
+          <div className="wind">
+            <GiBarbedSun className='windIcon' />
+            <div className="humidInfo">
+              <h2>2.35km/h</h2>
+              <p>velocidad</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </MainWrapper>
   )
 }
